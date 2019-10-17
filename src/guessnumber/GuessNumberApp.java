9748460adc1fdae298fef4b9579016e39f6f9eec
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-10-18 00:10:51 +0200

    Changing name of the reference.

--- src/guessnumber/GuessNumberApp.java
+++ src/guessnumber/GuessNumberApp.java
@@ -2,7 +2,7 @@ package guessnumber;
 
 public class GuessNumberApp {
     public static void main(String[] args) {
-        Number enterNumber = new Number();
-        enterNumber.getNumber();
+        Number number = new Number();
+        number.getNumber();
     }
 }

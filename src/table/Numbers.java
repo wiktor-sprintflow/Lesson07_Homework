af3dce184068525963d80cea9b89f14274e4cc8f
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-10-17 21:21:55 +0200

    Applying review comments. getNumbers() returns table of integers and it's called out from returnOddSum().

--- src/table/Numbers.java
+++ src/table/Numbers.java
@@ -3,19 +3,22 @@ package table;
 import java.util.Scanner;
 
 class Numbers {
-    private int[] numbers = new int[5];
 
-    void getNumbers() {
+    private int[] getNumbers() {
+        int[] numbers = new int[5];
         Scanner input = new Scanner(System.in);
+
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Podaj liczbę:");
             numbers[i] = input.nextInt();
             input.nextLine();
         }
         input.close();
+        return numbers;
     }
 
     int returnOddSum() {
+        int[] numbers = getNumbers();
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             if (i % 2 == 0) {
--- src/table/NumbersApp.java
+++ src/table/NumbersApp.java
@@ -3,7 +3,6 @@ package table;
 public class NumbersApp {
     public static void main(String[] args) {
         Numbers numbers = new Numbers();
-        numbers.getNumbers();
         System.out.println("Suma liczby pierwszej, trzeciej i piątej wynosi: " + numbers.returnOddSum());
     }
 }
